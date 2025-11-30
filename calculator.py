aa5b7341d0e593abcc032a43e5113ab5e8f816c5
aleebasitc-beep <aleebasitc@gmail.com>
2025-11-30 16:45:29 +0500

    made calculator

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print('---Welcome to the calculator---')
+while True:
+ num1 = int(input('enter number: '))
+ choose = input('choose (+, -, *, /): ')
+ num2 = int(input('enter number: '))
+ if choose == "+":
+  print(num1 + num2)
+ elif choose == "-":
+  print(num1 - num2)
+ elif choose == "*":
+  print(num1 * num2)
+ elif choose == "/":
+  if num2 == 0:
+   print('zero cannot be divided')
+  else: 
+   print(num1 / num2) 
+else:
+ print("invalidtry again!")  
